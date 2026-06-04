1e9ffe37f3bff7868133b7786893a7063cc33f63
csolanochi <carolinasolanochinchilla@gmail.com>
2026-06-03 19:59:07 -0600

    init (semana-2): configuración del proyecto — plantillas, imágenes y archivos multimedia
    Creación del repositorio y configuración inicial del proyecto.
    Se agregaron plantillas, imágenes y archivos multimedia como base del desarrollo

---
+++ InitializrSpringbootProject/src/test/java/com/tienda/tiendaTech/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.tiendaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
