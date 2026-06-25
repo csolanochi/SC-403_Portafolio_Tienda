658a91bb06473f809cc1d6639c225313e4c01033
csolanochi <csolanochi@gmail.com>
2026-06-25 17:54:08 -0600

    feat (semana-4): creacion de categorias, y fragmentos
    Nota:  Atraso de 2 semanas por daños en mi laptop

---
+++ InitializrSpringbootProject/src/main/java/com/tienda/tiendaTech/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.tienda.tiendaTech.repository;
+
+
+import com.tienda.tiendaTech.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+    public List<Categoria> findByActivoTrue();
+}
+ 
